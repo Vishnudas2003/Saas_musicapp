0d844ee008e5d2c60c2b9eae9a0d1c55940a0b8a
Vishnu Das Puthukudi <vishnudas7412@gmail.com>
2024-09-11 21:43:38 -0400

    Deleted the old one and create a new-app

--- tavimuzi/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hi there</h1>
-      </main>
-  );
-}
