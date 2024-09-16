5803c79da64500c0bbcb91e8378edb3d85847a07
Vishnu Das Puthukudi <vishnudas7412@gmail.com>
2024-09-16 17:27:01 -0400

    Updated with Next Auth andSession provider

--- my-app/app/components/Appbar.tsx
+++ my-app/app/components/Appbar.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import {signIn} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
 
 export function Appbar() {
+  const session = useSession();
   return <div>
     <div className="flex justify-between">
         <div>
             Music App
         </div>
         <div>
-            <button className="m-2 p-2 bg-blue-400"onClick={() => signIn()}>Sign in with Google</button>
+          {session.data?.user && <button className="m-2 p-2 bg-blue-400"onClick={() => signOut()}>Logout</button>}
+          {!session.data?.user && <button className="m-2 p-2 bg-blue-400"onClick={() => signIn()}>Sign in with Google</button>}        
         </div>
     </div>
   </div>
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { Appbar } from "./components/Appbar";
-
+console.log(process.env.GOOGLE_CLIENT_ID);
+console.log(process.env.GOOGLE_CLIENT_SECRET);
 export default function Home() {
   return (
       <main>
